6432450387e00363cc4a14b3eb35e4b142fed475
MonkDabaLegaro <monk.of.the.chaos@gmail.com>
2026-08-25 12:16:21 -0400

    fix: expose Room schema tooling

--- apps/android/build.gradle.kts
+++ apps/android/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.2.21-2.0.5" apply false
+    id("androidx.room") version "2.8.4" apply false
 }
